5a8e1ee59663fadd5211e9256e40f4f2c8bd4656
kmokmo19 <hernancamoretti0@gmail.com>
2025-08-29 15:13:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -54,4 +54,4 @@ function sortearAmigo() {
     lista.innerHTML = "";
 
     resultado.innerHTML = `<li> El amigo secreto es: <strong>${nombreSorteado}</strong></li>`;
-}// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+}
